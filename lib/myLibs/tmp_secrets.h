bb94bcf0437efa896365045a6ddddc98dbbe6b28
DeLi0v <109518046+DeLi0v@users.noreply.github.com>
2023-11-12 01:16:46 +0300

    Update tmp_secrets.h

--- lib/myLibs/tmp_secrets.h
+++ lib/myLibs/tmp_secrets.h
@@ -7,15 +7,18 @@
 
 /////////////////////////////////////////
 // Blynk server
+
 #define SERVER_ADDRESS "" // Адрес сервера Blynk
 #define SERVER_PORT 8080
 
 /////////////////////////////////////////
 // Blynk device
+
 #define AUTH ""
 
 ////////////////////////////////////////
 // Таймеры
+
 int IDt_reconnectBlynk; // ID таймера для перезагрузки
 int IDt_delayForDevice; // ID таймера для считывания данных с датчиков
 
